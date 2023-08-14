b5d22b59e9361a5720571e8f42436c71baa3cffd
Cecilia Teodorani <cecilia.teodorani@studio.unibo.it>
2023-08-14 12:12:11 +0200

    add correct client

---
+++ src/main/typescript/distributed-client/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <p>Bruno Esposito, Andrea Ingargiola, Cecilia Teodorani | Academic year 21-22</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: "Footer"
+  }
+</script>
---
+++ src/main/typescript/distributed-client/src/environment.ts
@@ -0,0 +1,7 @@
+/**
+ * The environment of this application.
+ */
+export class Environment {
+    /** The host of the application backend. */
+    public static readonly APPLICATION_BACKEND_HOST: string = "http://127.0.0.1:8080";
+}
